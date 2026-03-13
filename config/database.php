dc2a294ecab20e819cc0a57960eb5fa64d8e3453
Tan Dat <tandat05.dev@gmail.com>
2026-03-13 17:42:54 +0700

    connect database

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "127.0.0.1";
+$dbname = "food";
+$port = 3307;
+$username = "root";
+$password = "";
+
+$dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connect successfully";
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
